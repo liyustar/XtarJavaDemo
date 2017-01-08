12ef0e4e5ef36a246f28a1003dae460788e29a46
liyustar <liyustar@gmail.com>
2017-01-08 23:00:02 +0800

    test aop

--- springdemo/src/test/java/com/lyx/SpringdemoApplicationTests.java
+++ springdemo/src/test/java/com/lyx/SpringdemoApplicationTests.java
@@ -2,23 +2,45 @@ package com.lyx;
 
 import com.lyx.exception.PerformanceException;
 import com.lyx.iface.Performer;
+import com.lyx.iface.Thinker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.ApplicationArguments;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration( {"idol.xml"} )
 public class SpringdemoApplicationTests {
+
+	@Autowired
+	Performer duke;
+
+	@Autowired
+	Magician magician;
+
+	@Autowired
+	Thinker volunteer;
+
 
 	@Test
 	public void contextLoads() throws PerformanceException {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("idol.xml");
+		// ApplicationContext ctx = new ClassPathXmlApplicationContext("com/lyx/idol.xml");
+		// Performer duke = (Performer) ctx.getBean("duke");
+		duke.perform();
+	}
 
-		Performer pf = (Performer) ctx.getBean("duke");
-		pf.perform();
+	@Test
+	public void magicianShouldReadVolunteersMind() {
+//		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/lyx/idol.xml");
+//        Magician magician = (Magician) ctx.getBean("magician");
+//        Volunteer volunteer = (Volunteer) ctx.getBean("volunteer");
+
+		volunteer.thinkOfSomething("haha");
+		assertEquals("haha", magician.getThoughts());
 	}
 }
